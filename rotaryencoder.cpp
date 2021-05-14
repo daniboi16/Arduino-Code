db019e473ecf213913a0a2b51a3d461e15ab2e6b
Daniel Joseph Alapat <71063467+daniboi16@users.noreply.github.com>
2021-05-14 11:39:43 +0530

    Create rotaryencoder.cpp

---
+++ rotaryencoder.cpp
@@ -0,0 +1,34 @@
+// Hardware connections and code explanation can be found at : https://danielalapat.hashnode.dev/rotary-encoder
+// to impliemnt a volume knob with the Rotary encoder
+
+
+#define DT 1                //connect DT to digital pin 1
+#define CLK 0               //connect CLK to digital pin 0
+
+int vol=50;                 //variable for volume
+
+int currentCLK;             //variables for rotary encoder
+int lastCLK;                //variables for rotary encoder
+int currentDT;
+
+void setup() {
+  Serial.begin(9600);       //to start serial monitor
+  pinMode(DT,INPUT);        //setting pin as input
+  pinMode(CLK,INPUT);       //setting pin as input
+  lastCLK = digitalRead(CLK);                   //setting last state of CLK pin
+}
+
+void loop() {
+    currentCLK = digitalRead(CLK);              //get the current state of of CLK pin
+    if (currentCLK != lastCLK){                 //if the curent state is not the same as the preivious state it indicates movement  
+      currentDT=digitalRead(DT);
+      if (currentDT != currentCLK){             //once there is movement we check the other pin to see if it is clockwise of anticlockwise  
+        vol = vol - 1;                          //if anticlockwise we decerement the index
+        }
+      else{                                     //if clockwise we increment the index
+        vol = vol + 1;
+        }
+    Serial.println(vol);                       //display volume
+    lastCLK = currentCLK;                      //update last value of CLK
+    }
+}
