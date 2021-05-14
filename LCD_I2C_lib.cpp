b5f2d7ecd7f8e8f2f1d8a3593594798cc25d1856
Daniel Joseph Alapat <71063467+daniboi16@users.noreply.github.com>
2021-05-14 11:36:09 +0530

    Create LCD_I2C_lib.cpp

---
+++ LCD_I2C_lib.cpp
@@ -0,0 +1,19 @@
+// hardware connections and code explanation can be found at :  https://danielalapat.hashnode.dev/interfacing-lcd-with-i2c
+// impliment LCD using I2C adaptor to display "CAR" using LiquidCrystal_I2C library
+
+#include <Wire.h>                  //library for I2C communications
+#include <LiquidCrystal_I2C.h>     //library for LCD communications
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // initialise the I2C address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();                 // turn on backlight
+  lcd.setCursor(0,0);              // set cursor to (0,0)
+  lcd.print("CAR");                // to display "CAR"
+}
+
+void loop()
+{
+}
